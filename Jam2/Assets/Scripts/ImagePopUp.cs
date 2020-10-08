1c9041bc085bd1491eb83f5d2ec97a1fe8f5520e
Amanda <69984329+awfederi@users.noreply.github.com>
2020-10-07 21:58:50 -0700

    Added ImagePopUp Script
    in the testing scene, i put this on the last capsule on the far right, the one that lights up YELLOW. click it and the testclue image will pop up. if you press escape the image will disappear (although its a little finicky since pressing escape at first takes your cursor out of the scene window)

---
+++ Jam2/Assets/Scripts/ImagePopUp.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImagePopUp : MonoBehaviour
+{
+    [SerializeField] private Image clue;
+    private bool on = false;
+
+    void FixedUpdate()
+    {
+        if (on)
+        {
+            clue.enabled = true;
+        }
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            on = false;
+            clue.enabled = false;
+        }
+    }
+    public void popUp()
+    {
+        on = true;
+        
+    }
+
+}
