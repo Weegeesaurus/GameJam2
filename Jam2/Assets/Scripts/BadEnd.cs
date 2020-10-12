06cc2bbaf254a62abca439e0ea3eab7b592b46d2
jyhrowolf <60275277+cetancopeland@users.noreply.github.com>
2020-10-12 01:32:22 -0700

    fade out

--- Jam2/Assets/Scripts/BadEnd.cs
+++ Jam2/Assets/Scripts/BadEnd.cs
@@ -1,8 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
-
 public class BadEnd : MonoBehaviour
 {
     // Start is called before the first frame update
