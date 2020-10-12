32f8ebfbd3272b71473f804d3899c34172cf08b1
Weegeesaurus <>
2020-10-11 23:26:58 -0700

    lightss

---
+++ Jam2/Assets/Scripts/GarageDoor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GarageDoor : MonoBehaviour
+{
+    public int itemID;
+    public DialogueChain dialogue;
+    public DialogueChain lockedDia;
+    public void UseKey()
+    {
+        if (PlayerState.instance.items[itemID] == true)
+        {
+            PlayerState.instance.GarageLock = true;
+            lockedDia.StartDialogue();
+        }
+        else
+        {
+            dialogue.StartDialogue();
+        }
+    }
+}
