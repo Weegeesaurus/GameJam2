bc985deef20c12be7ffe12e3cf1157d8bb487c56
jennatark <jennartark@gmail.com>
2020-10-12 02:20:09 -0700

    added keycard

---
+++ Jam2/Assets/Scripts/cardReader.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cardReader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public doorM door;
+    public int itemID;
+    public GameObject redSphere;
+    public GameObject greenSphere;
+
+    private void Start()
+    { 
+        redSphere.SetActive(true);
+        greenSphere.SetActive(false);
+    }
+    public void UnlockDoor()
+    {
+        if (door.IsLocked)
+            if (PlayerState.instance.items[itemID] == true)
+            {
+                door.IsLocked = false;
+                door.Open();
+                redSphere.SetActive(false);
+                greenSphere.SetActive(true);
+            }
+    }
+}
