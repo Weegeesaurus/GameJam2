de3857756ce08d03b0a8a7d866e378bf9d65b584
jyhrowolf <60275277+cetancopeland@users.noreply.github.com>
2020-10-11 23:48:58 -0700

    ending

---
+++ Jam2/Assets/Scripts/GoodEnd.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoodEnd : MonoBehaviour
+{
+    void FixedUpdate()
+    {
+        if(transform.position.z > -29.5)
+        {
+            transform.position += transform.forward * Time.deltaTime * 0.25f;
+        }
+}
+}
