51833f12710ef8493d9ed0d08adff11b4f07287c
jyhrowolf <60275277+cetancopeland@users.noreply.github.com>
2020-10-12 00:29:18 -0700

    BADEND

--- Jam2/Assets/Scripts/BadEnd.cs
+++ Jam2/Assets/Scripts/BadEnd.cs
@@ -1,19 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BadEnd : MonoBehaviour
 {
     // Start is called before the first frame update
+    public DialogueChain dialogueChain;
+    private bool speach = false;
     void FixedUpdate()
     {
         if(transform.position.z > -29.5)
         {
             transform.position += transform.forward * Time.deltaTime * 0.25f;
+            if(!speach)
+            {
+                dialogueChain.StartDialogue();
+                speach = true;
+            }
         }
         else
-        {
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, 90, -90), Time.deltaTime * 2.5f);
-        }
     }
+
 }
