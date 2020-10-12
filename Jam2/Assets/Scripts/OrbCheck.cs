efa99643dcf5b05be6d4696e5538865eb18330c1
jyhrowolf <60275277+cetancopeland@users.noreply.github.com>
2020-10-11 20:02:43 -0700

    Office spoooks go brrrrrrrrrrrrrrrrrrrrr

---
+++ Jam2/Assets/Scripts/OrbCheck.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbCheck : MonoBehaviour
+{
+    public PlayerState playerState;
+    public OfficePAths guy;
+    public int itemID;
+    public void Unlock()
+    {
+        if(playerState.items[itemID] == true)
+        {
+            guy.Skip();
+            Destroy(gameObject);
+        }
+    }
+}
